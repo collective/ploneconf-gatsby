8972a9f804ff85ba3c5516fd0a587fc442a589e5
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-05-19 12:37:07 +0200

    initial work

--- src/components/TalkSubmission/index.js
+++ src/components/TalkSubmission/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Form from 'react-jsonschema-form';
-import CaptchaField from '../CaptchaField';
 
 class TalkSubmission extends React.Component {
   constructor(props) {
@@ -15,62 +14,6 @@ class TalkSubmission extends React.Component {
       submitted: false,
       error: null,
     };
-  }
-
-  componentDidMount() {
-    fetch(`${process.env.GATSBY_API_URL}/@talk-proposal`, {
-      headers: new Headers({ Accept: 'application/json' }),
-    })
-      .then(res => res.json())
-      .then(
-        result => {
-          const { schema } = result;
-          // add captcha to schema
-          schema.fieldsets[0].fields.push('captcha');
-          schema.properties.captcha = {
-            title: 'Captcha',
-            type: 'string',
-          };
-          const widgets = Object.keys(schema.properties).reduce(
-            (widgetsSchema, fieldId) => {
-              const field = schema.properties[fieldId];
-              const widget = field.widget;
-              if (field.mode === 'hidden') {
-                widgetsSchema[fieldId] = {
-                  'ui:widget': 'hidden',
-                };
-              } else if (widget) {
-                widgetsSchema[fieldId] = {
-                  'ui:widget': widget,
-                };
-              }
-              return widgetsSchema;
-            },
-            {},
-          );
-          widgets.captcha = {
-            'ui:widget': () => (
-              <CaptchaField onChange={this.handleCaptchaChange} />
-            ),
-          };
-          this.setState({
-            schema: schema,
-            uiSchema: {
-              ...widgets,
-              'ui:order': schema.fieldsets[0].fields,
-            },
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        error => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        },
-      );
   }
 
   handleCaptchaChange = value => {
