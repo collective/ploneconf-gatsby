b106753f0b401487d4a0a5e21940ed0fe6b5b3ad
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-05-23 08:52:10 +0200

    fix image upload

--- src/components/form/widgets/FileUploadWidget.js
+++ src/components/form/widgets/FileUploadWidget.js
@@ -1,35 +1,56 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { string, func } from 'prop-types';
 import Dropzone from 'react-dropzone';
 
-const FileUploadWidget = ({ value, id, handleUpdate }) => {
-  let file = '';
-  if (value) {
-    file = (
-      <div className="thumb">
-        <div className="thumb-inner">
-          <img src={value.preview} className="img" alt="" />
+class FileUploadWidget extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      uploadedFile: null,
+    };
+  }
+
+  render() {
+    const { value, id, handleUpdate } = this.props;
+    const { uploadedFile } = this.state;
+    let file = '';
+    if (uploadedFile) {
+      file = (
+        <div className="thumb">
+          <div className="thumb-inner">
+            {uploadedFile.name}
+            {/* <img src={uploadedFile.preview} className="img" alt="" /> */}
+          </div>
         </div>
-      </div>
+      );
+    }
+    return (
+      <Dropzone
+        onDrop={files => {
+          const reader = new FileReader();
+          reader.onload = event => {
+            handleUpdate({ id, value: event.target.result });
+          };
+          reader.readAsDataURL(files[0]);
+          this.setState({ uploadedFile: files[0] });
+        }}
+      >
+        {() => (
+          <section className="container">
+            <div className="dropzone">
+              <input />
+              <p>Drag and drop some files here, or click to select files</p>
+            </div>
+            <aside>
+              <h4>File</h4>
+              <aside className="thumbs-container">{file}</aside>
+            </aside>
+          </section>
+        )}
+      </Dropzone>
     );
   }
-  return (
-    <Dropzone onDrop={files => handleUpdate({ id, value: files[0] })}>
-      {() => (
-        <section className="container">
-          <div className="dropzone">
-            <input />
-            <p>Drag and drop some files here, or click to select files</p>
-          </div>
-          <aside>
-            <h4>File</h4>
-            <aside className="thumbs-container">{file}</aside>
-          </aside>
-        </section>
-      )}
-    </Dropzone>
-  );
-};
+}
 
 FileUploadWidget.propTypes = {
   id: string,
