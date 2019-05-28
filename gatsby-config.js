b992a86fe7c0c589b874433a7979b7b5e352ce3a
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-05-28 11:14:26 +0200

    add to head

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
       options: {
         trackingId: 'UA-140389816-1',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
