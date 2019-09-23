22022480e31eaf7a4d254ee717bbfe7f5e751348
Piero Nicolli <piero.nicolli@redturtle.it>
2019-09-23 11:12:50 +0200

    Updated keynoters ordering

--- src/components/schedule/ScheduleContainer/index.js
+++ src/components/schedule/ScheduleContainer/index.js
@@ -16,18 +16,19 @@ class ScheduleContainer extends React.Component {
       <div className="schedule-container">
         <div className="container">
           <h2>
-            Schedule: plan of <b>talks</b> and <b>keynotes</b>
+            Plan of <b>talks</b> and <b>keynotes</b>
           </h2>
           <p>
             This is the plan of talks and keynotes that we have selected to make
-            the Plone Conference a real digital experience. There's not only
-            Plone-related talks, but also talks from different web-related topic
-            like Volto, Guillotina, Pyramid, Javascript, databases and agile.
+            the Plone Conference a real digital experience. There&apos;s not
+            only Plone-related talks, but also talks from different web-related
+            topic like Volto, Guillotina, Pyramid, Javascript, databases and
+            agile.
           </p>
 
           <p>
-            All talks and keynotes will take place between 23 and 25 October
-            2019 at the Apollo Cinepark rooms in the city center of Ferrara.
+            All talks and keynotes will take place between 23rd and 25th October
+            2019 at the Apollo Cinepark in the city center of Ferrara.
           </p>
           <ScheduleKeynoters />
         </div>
